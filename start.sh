cf7803c1b78493a56f3271ac2023e4075a7372e5
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2022-05-13 14:44:30 +0200

    start: improve volume mount and pass FQDN

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 vopts=""
-if [ -d  $1 ]; then
+if [[ ! -z "$1" && -d "$1" ]]; then
     path=$(readlink -f $1)
     vopts="-v $path:/var/www/localhost/htdocs"
 fi
@@ -14,6 +14,6 @@ podman pod rm $POD
 podman pod create --replace --name $POD
 podman run --rm --detach --network=host --cap-add=NET_ADMIN --device /dev/net/tun -v ovpn-data:/etc/openvpn/:z --pod $POD --name $POD-vpn  ghcr.io/nethserver/nextsec-vpn:latest
 podman run --rm --detach --network=host --volumes-from=$POD-vpn --pod $POD --name $POD-proxy docker.io/library/traefik:v2.6 --providers.file.directory=/etc/openvpn/proxy/ --providers.file.watch=true --serversTransport.insecureSkipVerify=true --entryPoints.web.address=:8181 --accesslog=true --log.level=DEBUG
-podman run --rm --detach --network=host --volumes-from=$POD-vpn --pod $POD --name $POD-api  ghcr.io/nethserver/nextsec-api:latest
+podman run --rm --detach --network=host --volumes-from=$POD-vpn --pod $POD --name $POD-api -e FQDN=$(hostname -f) ghcr.io/nethserver/nextsec-api:latest
 podman run --rm --detach --network=host --pod $POD --name $POD-ui $vopts ghcr.io/nethserver/nextsec-ui:latest
 

6b2b08f5fed6eac0f51e2c2c5040f95e38ff8e0f
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2022-05-16 09:20:26 +0200

    start: fix startup orderd

--- start.sh
+++ start.sh
@@ -13,7 +13,8 @@ podman pod rm $POD
 
 podman pod create --replace --name $POD
 podman run --rm --detach --network=host --cap-add=NET_ADMIN --device /dev/net/tun -v ovpn-data:/etc/openvpn/:z --pod $POD --name $POD-vpn  ghcr.io/nethserver/nextsec-vpn:latest
-podman run --rm --detach --network=host --volumes-from=$POD-vpn --pod $POD --name $POD-proxy docker.io/library/traefik:v2.6 --providers.file.directory=/etc/openvpn/proxy/ --providers.file.watch=true --serversTransport.insecureSkipVerify=true --entryPoints.web.address=:8181 --accesslog=true --log.level=DEBUG
 podman run --rm --detach --network=host --volumes-from=$POD-vpn --pod $POD --name $POD-api -e FQDN=$(hostname -f) ghcr.io/nethserver/nextsec-api:latest
 podman run --rm --detach --network=host --pod $POD --name $POD-ui $vopts ghcr.io/nethserver/nextsec-ui:latest
+sleep 2
+podman run --rm --detach --network=host --volumes-from=$POD-vpn --pod $POD --name $POD-proxy docker.io/library/traefik:v2.6 --providers.file.directory=/etc/openvpn/proxy/ --providers.file.watch=true --serversTransport.insecureSkipVerify=true --entryPoints.web.address=:8181 --accesslog=true --log.level=DEBUG
 

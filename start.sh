4fca18cec61f17b4c51ac7e6a47e53722089df1c
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2022-05-25 09:16:40 +0200

    start: remove credentials file
    The file is now inside the volume

--- start.sh
+++ start.sh
@@ -11,7 +11,6 @@ POD=${POD-nextsec-pod}
 podman pod stop $POD
 podman pod rm $POD
 
-touch credentials.json
 podman pod create --replace --name $POD
 podman run --rm --detach --network=host --cap-add=NET_ADMIN --device /dev/net/tun -v ovpn-data:/etc/openvpn/:z --pod $POD --name $POD-vpn  ghcr.io/nethserver/nextsec-vpn:latest
 podman run --rm --detach --network=host --volumes-from=$POD-vpn --pod $POD --name $POD-api -e API_DEBUG=1 -e FQDN=$(hostname -f) ghcr.io/nethserver/nextsec-api:latest

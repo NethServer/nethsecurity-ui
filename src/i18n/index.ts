6bc44079e96cde495710c9043dd4838413f83c2c
Andrea Leardini <andre8244@gmail.com>
2023-06-13 14:40:57 +0200

    fix i18n

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,21 +1,3 @@
-import { createI18n } from 'vue-i18n'
-
-export async function loadI18n() {
-  const locale = navigator.language.substring(0, 2)
-  const translations = await loadLanguage(locale)
-  const messages: any = {}
-  messages[locale] = { ...translations }
-
-  console.log('messages', messages) ////
-
-  const i18n = createI18n({
-    legacy: false,
-    locale,
-    messages
-  })
-  return i18n
-}
-
 export async function loadLanguage(lang: string): Promise<any> {
   try {
     const response = await fetch(`/i18n/${lang}/translation.json`)
--- src/main.ts
+++ src/main.ts
@@ -17,8 +17,25 @@ app.use(router)
 import { loadFontAwesome } from './fontawesome'
 loadFontAwesome(app)
 
-import { loadI18n } from './i18n'
-const i18n = await loadI18n()
-app.use(i18n)
+// i18n
 
-app.mount('#app')
+import { createI18n } from 'vue-i18n'
+import { loadLanguage } from './i18n'
+
+loadI18n()
+
+async function loadI18n() {
+  const locale = navigator.language.substring(0, 2)
+  const translations = await loadLanguage(locale)
+  const messages: any = {}
+  messages[locale] = { ...translations }
+
+  const i18n = createI18n({
+    legacy: false,
+    locale,
+    messages
+  })
+
+  app.use(i18n)
+  app.mount('#app')
+}

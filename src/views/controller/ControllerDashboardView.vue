0df9d985838f4a72981ffd493432cf9eecf04f7f
Andrea Leardini <andre8244@gmail.com>
2023-06-13 15:59:33 +0200

    controller: minor enhancements

--- src/views/controller/ControllerDashboardView.vue
+++ src/views/controller/ControllerDashboardView.vue
@@ -7,6 +7,7 @@
 import { useUnitsStore } from '@/stores/controller/units'
 import { NeButton } from '@nethserver/vue-tailwind-lib'
 import { onMounted } from 'vue'
+import { isEmpty } from 'lodash'
 
 const unitsStore = useUnitsStore()
 
@@ -23,15 +24,15 @@ onMounted(() => {
     <div class="mb-4 text-lg">Units</div>
     <NeButton @click="unitsStore.getUnits" class="mb-4">Reload units</NeButton>
   </div>
-  <div v-for="(unit, index) in unitsStore.units" :key="index" class="flex gap-8">
+  <div v-for="(unit, index) in unitsStore.units" :key="index" class="flex gap-12">
     <template v-if="unit">
       <span>{{ unit.name }}</span>
-      <span>{{ unit.ipaddress }}</span>
-      <span>{{ unit.registered ? 'registered' : 'not registered' }}</span>
-      <span>{{ unit.vpn ? 'connected' : 'not connected' }}</span>
+      <span>{{ unit.ipaddress ? unit.ipaddress : '-' }}</span>
+      <span>{{ unit.registered ? 'Registered' : 'Not registered' }}</span>
+      <span>{{ isEmpty(unit.vpn) ? 'Not connected' : 'Connected' }}</span>
     </template>
   </div>
-  <div class="mt-4">
+  <div class="mt-10">
     {{ unitsStore.units }}
   </div>
 </template>

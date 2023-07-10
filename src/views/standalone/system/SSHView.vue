edb59544df6e9fcc858ea22839faaace509bf4af
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2023-07-10 14:48:26 +0200

    feat(ssh): added SshConfig component to Ssh page

--- src/views/standalone/system/SSHView.vue
+++ src/views/standalone/system/SSHView.vue
@@ -3,14 +3,16 @@
   SPDX-License-Identifier: GPL-3.0-or-later
 -->
 
-<script setup lang="ts">
+<script lang="ts" setup>
 import { useI18n } from 'vue-i18n'
+import SshConfig from '@/components/standalone/ssh/SshConfig.vue'
 
 const { t } = useI18n()
 </script>
 
 <template>
-  <div>
-    <h1>{{ t('standalone.ssh.title') }}</h1>
+  <h1 class="page-title">{{ t('standalone.ssh.title') }}</h1>
+  <div class="max-w-3xl flex flex-col gap-y-8">
+    <SshConfig />
   </div>
 </template>

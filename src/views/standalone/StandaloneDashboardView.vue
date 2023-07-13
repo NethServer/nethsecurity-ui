a8a8dd0cb100e32c31f84cb73ad72d03f6f03127
Andrea Leardini <andre8244@gmail.com>
2023-07-13 12:55:43 +0200

    standalone: minor fixes

--- src/views/standalone/StandaloneDashboardView.vue
+++ src/views/standalone/StandaloneDashboardView.vue
@@ -5,22 +5,12 @@
 
 <script setup lang="ts">
 import { NeTitle, NeButton } from '@nethserver/vue-tailwind-lib'
-import { ref } from 'vue'
-import { ubusCall } from '@/lib/standalone/ubus'
 import { useI18n } from 'vue-i18n'
 import { useLoginStore } from '@/stores/standalone/standaloneLogin'
 import { savePreference } from '@nethserver/vue-tailwind-lib'
 
 const loginStore = useLoginStore()
 const { t } = useI18n()
-
-let ubusOutput = ref('')
-
-async function testUbus() {
-  ubusOutput.value = await ubusCall('luci', 'getRealtimeStats', {
-    mode: 'conntrack'
-  })
-}
 
 async function changeLocale(lang: string) {
   savePreference('locale', lang, loginStore.username)
@@ -35,12 +25,7 @@ async function changeLocale(lang: string) {
 
   <!-- ////  -->
   <div class="mb-8">
-    <NeButton @click="changeLocale('it')" class="mb-4 mr-4">ITA</NeButton>
-    <NeButton @click="changeLocale('en')" class="mb-4">ENG</NeButton>
+    <NeButton size="lg" @click="changeLocale('it')" class="mb-4 mr-4">ITA</NeButton>
+    <NeButton size="lg" @click="changeLocale('en')" class="mb-4">ENG</NeButton>
   </div>
-
-  <!-- ////  -->
-  <NeButton @click="testUbus" class="mb-4">Test ubus</NeButton>
-
-  <div>{{ ubusOutput }}</div>
 </template>

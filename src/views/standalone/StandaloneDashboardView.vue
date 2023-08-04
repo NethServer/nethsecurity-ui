0763ce7e1cb011b68505dc0a8e942a64fb70f269
Andrea Leardini <andre8244@gmail.com>
2023-08-04 15:01:33 +0200

    Test api: ns.templates add_ipv6_rules

--- src/views/standalone/StandaloneDashboardView.vue
+++ src/views/standalone/StandaloneDashboardView.vue
@@ -8,15 +8,28 @@ import { NeTitle, NeButton } from '@nethserver/vue-tailwind-lib'
 import { useI18n } from 'vue-i18n'
 import { useLoginStore } from '@/stores/standalone/standaloneLogin'
 import { savePreference } from '@nethserver/vue-tailwind-lib'
+import { ubusCall } from '@/lib/standalone/ubus'
+import { onBeforeRouteUpdate } from 'vue-router'
+import { ref } from 'vue'
 
 const loginStore = useLoginStore()
 const { t } = useI18n()
+
+const output = ref('')
 
 async function changeLocale(lang: string) {
   savePreference('locale', lang, loginStore.username)
 
   // reload page
   location.reload()
+}
+
+async function test() {
+  const res = await ubusCall('ns.templates', 'add_ipv6_rules') ////
+
+  console.log('res', res) ////
+
+  output.value = res.data
 }
 </script>
 
@@ -28,4 +41,8 @@ async function changeLocale(lang: string) {
     <NeButton size="lg" @click="changeLocale('it')" class="mb-4 mr-4">ITA</NeButton>
     <NeButton size="lg" @click="changeLocale('en')" class="mb-4">ENG</NeButton>
   </div>
+
+  <NeButton size="lg" @click="test" class="mt-8">ns.templates add_ipv6_rules</NeButton>
+  <div class="mt-4">Output:</div>
+  <div>{{ output }}</div>
 </template>

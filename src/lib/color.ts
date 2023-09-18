41ab7924fa88cecfa2bdec84ed8c6f7f343dab2f
Andrea Leardini <andre8244@gmail.com>
2023-09-18 17:05:06 +0200

    fix(dashboard): improve chart color

--- src/lib/color.ts
+++ src/lib/color.ts
@@ -3,6 +3,7 @@
 
 // Use these color constants only where Tailwind classes cannot be used
 export const CYAN_400 = '#22d3ee'
+export const CYAN_600 = '#0891b2'
 export const GRAY_500 = '#6b7280'
 export const GRAY_200 = '#374151'
 export const GRAY_700 = '#e5e7eb'

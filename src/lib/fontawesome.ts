2c209d263b25cabe4daf82fe3cdbdfb78ccc491b
Nico <36412026+nicolorevelli@users.noreply.github.com>
2023-10-20 09:33:26 +0200

    feat: add hotspot page (#52)

--- src/lib/fontawesome.ts
+++ src/lib/fontawesome.ts
@@ -6,7 +6,8 @@ import {
   faPowerOff,
   faCircleStop,
   faHouse as fasHouse,
-  faUserGear
+  faUserGear,
+  faWifi
 } from '@fortawesome/free-solid-svg-icons'
 import { faHouse as falHouse } from '@nethesis/nethesis-light-svg-icons'
 import { faServer as fasServer } from '@fortawesome/free-solid-svg-icons'
@@ -132,6 +133,7 @@ export async function loadFontAwesome(app: any) {
   library.add(faClone)
   library.add(faUserGear)
   library.add(faCircleStop)
+  library.add(faWifi)
   library.add(faHardDrive)
   library.add(faCircleChevronDown)
   library.add(faCircleChevronUp)

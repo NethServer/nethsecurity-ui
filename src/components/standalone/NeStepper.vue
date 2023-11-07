c3ee1a520fb798f3f3566ed232350491cc01a5e9
Kevin Cela <kevin.cela@aidaptsrl.com>
2023-11-07 10:20:58 +0000

    feat: add generic component for steps

---
+++ src/components/standalone/NeStepper.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { NeProgressBar } from '@nethserver/vue-tailwind-lib'
+import { range } from 'lodash'
+
+const props = defineProps<{
+  totalSteps: number
+  currentStep: number
+  stepLabel: string
+}>()
+</script>
+
+<template>
+  <div>
+    <NeProgressBar :progress="(currentStep / totalSteps) * 100" size="sm" />
+    <div class="mt-2 flex flex-row">
+      <div v-for="i in range(1, totalSteps + 1)" :key="i" class="flex grow basis-0 justify-center">
+        <p class="text-xs font-semibold text-primary-400" v-if="i == currentStep">
+          Step ({{ currentStep }}/{{ totalSteps }})
+        </p>
+      </div>
+    </div>
+  </div>
+</template>

f725205755085419ccf59f1d91132f3a13e7018b
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2023-11-15 11:17:40 +0100

    chore(deps): updated dependencies
    Updated node to 20.9.0
    Updated package-lock
    Updated icon definitions

--- src/lib/fontawesome.ts
+++ src/lib/fontawesome.ts
@@ -1,89 +1,93 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import {
-  faArrowRight,
-  faArrowsRotate,
-  faPowerOff,
-  faCircleStop,
-  faHouse as fasHouse,
-  faUserGear,
   faArrowCircleDown,
   faArrowCircleUp,
-  faArrowUpRightFromSquare,
+  faArrowRight,
   faArrowRotateLeft,
+  faArrowsLeftRight as fasArrowsLeftRight,
+  faArrowsRotate,
+  faArrowUpRightFromSquare,
+  faBan as fasBan,
+  faBars as fasBars,
+  faBolt as fasBolt,
   faBoxArchive,
-  faPlay,
-  faRotate,
-  faWifi,
+  faChartLine as fasChartLine,
+  faCheck as fasCheck,
+  faChevronDown as fasChevronDown,
+  faChevronUp as fasChevronUp,
+  faCircleArrowDown,
+  faCircleArrowUp,
+  faCircleCheck as fasCircleCheck,
+  faCircleChevronDown,
+  faCircleChevronUp,
+  faCircleHalfStroke as fasCircleHalfStroke,
+  faCircleInfo as fasCircleInfo,
+  faCircleMinus as fasCircleMinus,
+  faCirclePlus as fasCirclePlus,
+  faCircleQuestion as fasCircleQuestion,
+  faCircleStop,
+  faCircleUser as fasCircleUser,
+  faCircleXmark as fasCircleXmark,
+  faClock as faClock,
+  faClone as faClone,
+  faCopy as fasCopy,
   faDiagramProject as fasDiagramProject,
-  faArrowsLeftRight as fasArrowsLeftRight
+  faDownLeftAndUpRightToCenter,
+  faEarthAmericas as fasEarthAmericas,
+  faEllipsisVertical as fasEllipsisVertical,
+  faFloppyDisk as fasFloppyDisk,
+  faGear as fasGear,
+  faGlobe as fasGlobe,
+  faHardDrive,
+  faHouse as fasHouse,
+  faKey,
+  faLayerGroup as faLayerGroup,
+  faList as fasList,
+  faLocationDot as fasLocationDot,
+  faLock as faLock,
+  faMagnifyingGlass as fasMagnifyingGlass,
+  faMoon as fasMoon,
+  faNetworkWired as fasNetworkWired,
+  faPenToSquare as fasPenToSquare,
+  faPlay,
+  faPlus as fasPlus,
+  faPowerOff,
+  faRotate,
+  faScaleBalanced as faScaleBalanced,
+  faServer as fasServer,
+  faShapes as fasShapes,
+  faShield as fasShield,
+  faShieldHalved as fasShieldHalved,
+  faStar as fasStar,
+  faSun as fasSun,
+  faTrash as fasTrash,
+  faUnlock as fasUnlock,
+  faUpRightAndDownLeftFromCenter,
+  faUserGear,
+  faUserGroup as fasUserGroup,
+  faUsers as fasUsers,
+  faWarning as faWarning,
+  faWifi,
+  faWrench as fasWrench,
+  faXmark as fasXmark
 } from '@fortawesome/free-solid-svg-icons'
-import { faHouse as falHouse } from '@nethesis/nethesis-light-svg-icons'
-import { faServer as fasServer } from '@fortawesome/free-solid-svg-icons'
-import { faServer as falServer } from '@nethesis/nethesis-light-svg-icons'
-import { faNetworkWired as fasNetworkWired } from '@fortawesome/free-solid-svg-icons'
-import { faNetworkWired as falNetworkWired } from '@nethesis/nethesis-light-svg-icons'
-import { faUserGroup as fasUserGroup } from '@fortawesome/free-solid-svg-icons'
-import { faUserGroup as falUserGroup } from '@nethesis/nethesis-light-svg-icons'
-import { faBlockBrickFire as fasBlockBrickFire } from '@nethesis/nethesis-solid-svg-icons'
-import { faBlockBrickFire as falBlockBrickFire } from '@nethesis/nethesis-light-svg-icons'
-import { faShieldHalved as fasShieldHalved } from '@fortawesome/free-solid-svg-icons'
-import { faShieldHalved as falShieldHalved } from '@nethesis/nethesis-light-svg-icons'
-import { faGlobe as fasGlobe } from '@fortawesome/free-solid-svg-icons'
-import { faGlobe as falGlobe } from '@nethesis/nethesis-light-svg-icons'
-import { faList as fasList } from '@fortawesome/free-solid-svg-icons'
-import { faList as falList } from '@nethesis/nethesis-light-svg-icons'
-import { faChartLine as fasChartLine } from '@fortawesome/free-solid-svg-icons'
-import { faChartLine as falChartLine } from '@nethesis/nethesis-light-svg-icons'
-import { faMagnifyingGlass as fasMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
-import { faCircleQuestion as fasCircleQuestion } from '@fortawesome/free-solid-svg-icons'
-import { faMoon as fasMoon } from '@fortawesome/free-solid-svg-icons'
-import { faSun as fasSun } from '@fortawesome/free-solid-svg-icons'
-import { faCircleHalfStroke as fasCircleHalfStroke } from '@fortawesome/free-solid-svg-icons'
-import { faCircleUser as fasCircleUser } from '@fortawesome/free-solid-svg-icons'
-import { faChevronDown as fasChevronDown } from '@fortawesome/free-solid-svg-icons'
-import { faChevronUp as fasChevronUp } from '@fortawesome/free-solid-svg-icons'
-import { faXmark as fasXmark } from '@fortawesome/free-solid-svg-icons'
-import { faGear as fasGear } from '@fortawesome/free-solid-svg-icons'
-import { faBars as fasBars } from '@fortawesome/free-solid-svg-icons'
-import { faTrash as fasTrash } from '@fortawesome/free-solid-svg-icons'
-import { faFloppyDisk as fasFloppyDisk } from '@fortawesome/free-solid-svg-icons'
-import { faPlus as fasPlus } from '@fortawesome/free-solid-svg-icons'
-import { faPenToSquare as fasPenToSquare } from '@fortawesome/free-solid-svg-icons'
-import { faCirclePlus as fasCirclePlus } from '@fortawesome/free-solid-svg-icons'
-import { faCircleMinus as fasCircleMinus } from '@fortawesome/free-solid-svg-icons'
-import { faCircleXmark as fasCircleXmark } from '@fortawesome/free-solid-svg-icons'
-import { faCircleCheck as fasCircleCheck } from '@fortawesome/free-solid-svg-icons'
-import { faLocationDot as fasLocationDot } from '@fortawesome/free-solid-svg-icons'
-import { faEarthAmericas as fasEarthAmericas } from '@fortawesome/free-solid-svg-icons'
-import { faEllipsisVertical as fasEllipsisVertical } from '@fortawesome/free-solid-svg-icons'
-import { faCopy as fasCopy } from '@fortawesome/free-solid-svg-icons'
-import { faUnlock as fasUnlock } from '@fortawesome/free-solid-svg-icons'
-import { faUsers as fasUsers } from '@fortawesome/free-solid-svg-icons'
-import { faScaleBalanced as faScaleBalanced } from '@fortawesome/free-solid-svg-icons'
-import { faClock as faClock } from '@fortawesome/free-solid-svg-icons'
-import { faLayerGroup as faLayerGroup } from '@fortawesome/free-solid-svg-icons'
-import { faLock as faLock } from '@fortawesome/free-solid-svg-icons'
-import { faWarning as faWarning } from '@fortawesome/free-solid-svg-icons'
-import { faWrench as fasWrench } from '@fortawesome/free-solid-svg-icons'
-import { faCheck as fasCheck } from '@fortawesome/free-solid-svg-icons'
-import { faBolt as fasBolt } from '@fortawesome/free-solid-svg-icons'
-import { faBan as fasBan } from '@fortawesome/free-solid-svg-icons'
-import { faShield as fasShield } from '@fortawesome/free-solid-svg-icons'
-import { faCircleInfo as fasCircleInfo } from '@fortawesome/free-solid-svg-icons'
-import { faEmptySet } from '@nethesis/nethesis-solid-svg-icons'
-import { faClone as faClone } from '@fortawesome/free-solid-svg-icons'
-import { faHardDrive } from '@fortawesome/free-solid-svg-icons/faHardDrive'
-import { faCircleChevronDown } from '@fortawesome/free-solid-svg-icons/faCircleChevronDown'
-import { faCircleChevronUp } from '@fortawesome/free-solid-svg-icons/faCircleChevronUp'
-import { faDownLeftAndUpRightToCenter } from '@fortawesome/free-solid-svg-icons/faDownLeftAndUpRightToCenter'
-import { faUpRightAndDownLeftFromCenter } from '@fortawesome/free-solid-svg-icons/faUpRightAndDownLeftFromCenter'
-import { faCircleArrowDown } from '@fortawesome/free-solid-svg-icons/faCircleArrowDown'
-import { faCircleArrowUp } from '@fortawesome/free-solid-svg-icons/faCircleArrowUp'
-import { faShapes as fasShapes } from '@fortawesome/free-solid-svg-icons'
-import { faTrafficCone as fasTrafficCone } from '@nethesis/nethesis-solid-svg-icons'
-import { faStar as fasStar } from '@fortawesome/free-solid-svg-icons'
-
+import {
+  faBlockBrickFire as falBlockBrickFire,
+  faChartLine as falChartLine,
+  faGlobe as falGlobe,
+  faHouse as falHouse,
+  faList as falList,
+  faNetworkWired as falNetworkWired,
+  faServer as falServer,
+  faShieldHalved as falShieldHalved,
+  faUserGroup as falUserGroup
+} from '@nethesis/nethesis-light-svg-icons'
+import {
+  faBlockBrickFire as fasBlockBrickFire,
+  faEmptySet,
+  faTrafficCone as fasTrafficCone
+} from '@nethesis/nethesis-solid-svg-icons'
 import {
   faAmazon,
   faFacebook,
@@ -97,9 +101,8 @@ import {
   faVimeo,
   faWhatsapp,
   faYoutube
-} from '@nethesis/nethesis-brands-svg-icons'
-import { faKey } from '@fortawesome/free-solid-svg-icons/faKey'
-import { faCalendar } from '@fortawesome/free-solid-svg-icons/faCalendar'
+} from '@fortawesome/free-brands-svg-icons'
+import { faNetflix, faNordVpn, faSquareXTwitter } from '@nethesis/nethesis-brands-svg-icons'
 
 export async function loadFontAwesome(app: any) {
   app.component('FontAwesomeIcon', FontAwesomeIcon)
@@ -187,8 +190,8 @@ export async function loadFontAwesome(app: any) {
   library.add(faAmazon)
   library.add(faWhatsapp)
   library.add(faInstagram)
-  // library.add(faNetflix) ////
-  // library.add(faXTwitter) ////
+  library.add(faNetflix)
+  library.add(faSquareXTwitter)
   library.add(faTelegram)
   library.add(faTiktok)
   library.add(faYoutube)
@@ -196,7 +199,7 @@ export async function loadFontAwesome(app: any) {
   library.add(faVimeo)
   library.add(faSnapchat)
   library.add(faPinterest)
-  // library.add(faNordVpn) ////
+  library.add(faNordVpn)
   library.add(faTwitch)
   library.add(fasTrafficCone)
   library.add(fasStar)

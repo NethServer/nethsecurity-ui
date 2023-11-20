e491197763ed6614160f3f7b3b4248167e5eb37a
Kevin Cela <kevin.cela@aidaptsrl.com>
2023-11-20 09:59:12 +0000

    feat(reverse-proxy): add page skeleton

---
+++ src/views/standalone/network/ReverseProxyView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+import { NeTitle } from '@nethserver/vue-tailwind-lib'
+
+const { t } = useI18n()
+</script>
+
+<template>
+  <NeTitle>{{ t('standalone.reverse_proxy.title') }}</NeTitle>
+</template>

e9075c7f9c64119407651a43ca866ea3c62c0eb9
Kevin Cela <kevin.cela@aidaptsrl.com>
2023-11-20 10:57:39 +0000

    feat(reverse-proxy): add table and delete modal

---
+++ src/components/standalone/reverse_proxy/DeleteProxyModal.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+import { NeModal, NeInlineNotification, getAxiosErrorMessage } from '@nethserver/vue-tailwind-lib'
+import { ref, toRefs } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { ubusCall } from '@/lib/standalone/ubus'
+import type { ReverseProxy } from '@/views/standalone/network/ReverseProxyView.vue'
+
+const { t } = useI18n()
+
+const props = defineProps<{
+  visible: boolean
+  itemToDelete: ReverseProxy | null
+}>()
+
+const emit = defineEmits(['close', 'proxy-deleted'])
+
+const { visible, itemToDelete } = toRefs(props)
+const error = ref({
+  notificationDescription: '',
+  notificationDetails: ''
+})
+const isDeleting = ref(false)
+
+async function deleteProxy() {
+  if (itemToDelete.value) {
+    try {
+      error.value = {
+        notificationDescription: '',
+        notificationDetails: ''
+      }
+      isDeleting.value = true
+      await ubusCall('ns.reverseproxy', 'delete-proxy', { id: itemToDelete.value.id })
+      emit('proxy-deleted')
+      emit('close')
+    } catch (err: any) {
+      error.value.notificationDescription = t(getAxiosErrorMessage(err))
+      error.value.notificationDetails = err.toString()
+    } finally {
+      isDeleting.value = false
+    }
+  }
+}
+
+function close() {
+  error.value = {
+    notificationDescription: '',
+    notificationDetails: ''
+  }
+  emit('close')
+}
+</script>
+
+<template>
+  <NeModal
+    :visible="visible"
+    kind="warning"
+    :title="t('standalone.reverse_proxy.delete_proxy')"
+    :primaryLabel="t('common.delete')"
+    :primaryButtonDisabled="isDeleting"
+    :primaryButtonLoading="isDeleting"
+    @primaryClick="deleteProxy()"
+    @close="close()"
+  >
+    {{
+      t('standalone.reverse_proxy.delete_proxy_message', {
+        path: itemToDelete?.location ?? ''
+      })
+    }}
+    <NeInlineNotification
+      v-if="error.notificationDescription"
+      kind="error"
+      :title="t('error.cannot_delete_proxy')"
+      :description="error.notificationDescription"
+      class="my-2"
+      ><template #details v-if="error.notificationDetails">
+        {{ error.notificationDetails }}
+      </template></NeInlineNotification
+    >
+  </NeModal>
+</template>
---
+++ src/components/standalone/reverse_proxy/ProxyTable.vue
@@ -0,0 +1,81 @@
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+import NeTable from '../NeTable.vue'
+import { NeDropdown, NeButton } from '@nethserver/vue-tailwind-lib'
+import type { ReverseProxy } from '@/views/standalone/network/ReverseProxyView.vue'
+
+const { t } = useI18n()
+
+defineProps<{
+  proxies: ReverseProxy[]
+}>()
+
+const emit = defineEmits(['proxy-delete', 'proxy-edit'])
+
+const tableHeaders = [
+  {
+    label: t('standalone.reverse_proxy.domain_path'),
+    key: 'domain_path'
+  },
+  {
+    label: t('standalone.reverse_proxy.destination_url'),
+    key: 'destination'
+  },
+  {
+    label: t('standalone.reverse_proxy.description'),
+    key: 'description'
+  },
+  {
+    label: t('standalone.reverse_proxy.allowed_networks'),
+    key: 'allow'
+  },
+  {
+    label: '',
+    key: 'menu'
+  }
+]
+
+function getDropdownItems(item: ReverseProxy) {
+  return [
+    {
+      id: 'delete',
+      label: t('common.delete'),
+      iconStyle: 'fas',
+      icon: 'trash',
+      danger: true,
+      action: () => {
+        emit('proxy-delete', item)
+      }
+    }
+  ]
+}
+</script>
+
+<template>
+  <NeTable :data="proxies" :headers="tableHeaders">
+    <template #domain_path="{ item }: { item: ReverseProxy }">
+      <p>{{ item.domain ?? item.path ?? item.location }}</p>
+    </template>
+    <template #description="{ item }: { item: ReverseProxy }">
+      <p>{{ item.description ? item.description : '-' }}</p>
+    </template>
+    <template #allow="{ item }: { item: ReverseProxy }">
+      <p>{{ item.allow?.join(', ') ?? '-' }}</p>
+    </template>
+    <template #menu="{ item }: { item: ReverseProxy }">
+      <div class="align-center flex justify-end">
+        <NeButton kind="tertiary" @click="emit('proxy-edit', item)">
+          <template #prefix>
+            <font-awesome-icon
+              :icon="['fas', 'pen-to-square']"
+              class="h-4 w-4"
+              aria-hidden="true"
+            />
+          </template>
+          {{ t('common.edit') }}
+        </NeButton>
+        <NeDropdown :items="getDropdownItems(item)" :align-to-right="true" />
+      </div>
+    </template>
+  </NeTable>
+</template>

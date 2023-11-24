ea4c4e4b6e5c611c235dcea3b3f39de932a560df
Andrea Leardini <andrea.leardini@nethesis.it>
2023-11-24 14:58:43 +0100

    feat: merge pull request #90 from NethServer/notification-engine
    * feat: show toast notifications on api error
    
    * fix: add @types/lodash-es to dependencies

---
+++ src/components/standalone/ToastNotificationsArea.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useNotificationsStore } from '../../stores/standalone/notifications'
+import NeAxiosErrorModal from './NeAxiosErrorModal.vue'
+import { NeToastNotification } from '@nethserver/vue-tailwind-lib'
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+const notificationsStore = useNotificationsStore()
+
+const notificationsToShow = computed(() => {
+  return notificationsStore.notifications.filter((notification) => notification.isShown)
+})
+</script>
+
+<template>
+  <div>
+    <div
+      aria-live="assertive"
+      class="pointer-events-none fixed inset-0 flex items-start px-4 pb-6 pt-20 text-sm"
+    >
+      <div class="flex w-full flex-col items-end space-y-4">
+        <TransitionGroup name="fade">
+          <NeToastNotification
+            v-for="notification in notificationsToShow"
+            :key="notification.id"
+            :notification="notification"
+            :srCloseLabel="t('common.close')"
+            showCloseButton
+            @close="notificationsStore.hideNotification(notification.id)"
+          />
+        </TransitionGroup>
+      </div>
+    </div>
+    <!-- axios error modal -->
+    <NeAxiosErrorModal />
+  </div>
+</template>

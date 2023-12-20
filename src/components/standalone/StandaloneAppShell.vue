984149137efb4da41ecc8e709ee55cc3eb8e46e9
Andrea Leardini <andre8244@gmail.com>
2023-12-20 14:40:40 +0100

    style: clean up StandaloneAppShell code

--- src/components/standalone/StandaloneAppShell.vue
+++ src/components/standalone/StandaloneAppShell.vue
@@ -135,7 +135,7 @@ function openNotificationsDrawer() {
                   </button>
                 </div>
               </TransitionChild>
-              <!-- Sidebar component, swap this element with another sidebar if you like -->
+              <!-- Sidebar component -->
               <div
                 class="flex grow flex-col gap-y-5 overflow-y-auto bg-white px-2 pb-4 dark:bg-gray-950"
               >
@@ -154,42 +154,6 @@ function openNotificationsDrawer() {
                         <SideMenu />
                       </ul>
                     </li>
-                    <!-- <li> //// 
-                      <div class="text-xs font-semibold leading-6 text-gray-400">Your teams</div>
-                      <ul role="list" class="-mx-2 mt-2 space-y-1">
-                        <li v-for="team in teams" :key="team.name">
-                          <a
-                            :href="team.href"
-                            :class="[
-                              team.current
-                                ? 'bg-gray-50 text-primary-600'
-                                : 'text-gray-700 hover:text-primary-600 hover:bg-gray-50',
-                              'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
-                            ]"
-                          >
-                            <span
-                              :class="[
-                                team.current
-                                  ? 'text-primary-600 border-primary-600'
-                                  : 'text-gray-400 border-gray-200 group-hover:border-primary-600 group-hover:text-primary-600',
-                                'flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white'
-                              ]"
-                              >{{ team.initial }}</span
-                            >
-                            <span class="truncate">{{ team.name }}</span>
-                          </a>
-                        </li>
-                      </ul>
-                    </li> -->
-                    <li class="mt-auto">
-                      <!-- //// use tertiary button with icon -->
-                      <!-- <a
-                        href="#"
-                        class="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-primary-700 hover:bg-gray-100 hover:text-primary-800 dark:text-primary-500 dark:hover:bg-gray-900 dark:hover:text-primary-500"
-                      >
-                        Minimize
-                      </a> -->
-                    </li>
                   </ul>
                 </nav>
               </div>
@@ -202,7 +166,7 @@ function openNotificationsDrawer() {
     <!-- Static sidebar for desktop -->
     <!-- //// removed lg:inset-y-0 class -->
     <div class="hidden lg:fixed lg:z-50 lg:flex lg:h-screen lg:w-72 lg:flex-col">
-      <!-- Sidebar component, swap this element with another sidebar if you like -->
+      <!-- Sidebar component -->
       <div
         class="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-2 pb-4 dark:border-gray-700 dark:bg-gray-950"
       >
@@ -219,42 +183,6 @@ function openNotificationsDrawer() {
               <ul role="list" class="space-y-2">
                 <SideMenu />
               </ul>
-            </li>
-            <!-- <li> //// 
-              <div class="text-xs font-semibold leading-6 text-gray-400">Your teams</div>
-              <ul role="list" class="-mx-2 mt-2 space-y-1">
-                <li v-for="team in teams" :key="team.name">
-                  <a
-                    :href="team.href"
-                    :class="[
-                      team.current
-                        ? 'bg-gray-50 text-primary-600'
-                        : 'text-gray-700 hover:text-primary-600 hover:bg-gray-50',
-                      'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
-                    ]"
-                  >
-                    <span
-                      :class="[
-                        team.current
-                          ? 'text-primary-600 border-primary-600'
-                          : 'text-gray-400 border-gray-200 group-hover:border-primary-600 group-hover:text-primary-600',
-                        'flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white'
-                      ]"
-                      >{{ team.initial }}</span
-                    >
-                    <span class="truncate">{{ team.name }}</span>
-                  </a>
-                </li>
-              </ul>
-            </li> -->
-            <li class="mt-auto">
-              <!-- //// use tertiary button with icon -->
-              <!-- <a
-                href="#"
-                class="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-primary-700 hover:bg-gray-100 hover:text-primary-800 dark:text-primary-500 dark:hover:bg-gray-900 dark:hover:text-primary-500"
-              >
-                Minimize
-              </a> -->
             </li>
           </ul>
         </nav>
@@ -390,17 +318,7 @@ function openNotificationsDrawer() {
                   class="h-6 w-6 shrink-0"
                   aria-hidden="true"
                 />
-                <!-- <img //// 
-                  class="h-8 w-8 rounded-full bg-gray-50"
-                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt=""
-                /> -->
                 <span class="hidden lg:flex lg:items-center">
-                  <!-- <span //// 
-                    class="ml-4 text-sm font-semibold leading-6 text-gray-900"
-                    aria-hidden="true"
-                    >Tom Cook</span
-                  > -->
                   <font-awesome-icon
                     :icon="['fas', 'chevron-down']"
                     class="ml-2 h-3 w-3 shrink-0"

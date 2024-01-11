4d200a304d7734655c43936335ee78ef7169f1d9
Kevin Cela <kevin.cela@aidaptsrl.com>
2024-01-11 16:08:39 +0000

    feat: add base certificates view and table (wip)

---
+++ src/components/standalone/certificates/CertificatesTable.vue
@@ -0,0 +1,129 @@
+<!--
+  Copyright (C) 2023 Nethesis S.r.l.
+  SPDX-License-Identifier: GPL-3.0-or-later
+-->
+
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+import NeTable from '../NeTable.vue'
+import { NeButton, NeDropdown, NeBadge, NeTooltip } from '@nethserver/vue-tailwind-lib'
+import type { Certificate } from '@/views/standalone/system/CertificatesView.vue'
+
+const { t } = useI18n()
+
+defineProps<{
+  certificates: Certificate[]
+}>()
+
+const emit = defineEmits<{
+  delete: [item: Certificate]
+  edit: [item: Certificate]
+  setAsDefault: [item: Certificate]
+  showCertificate: [item: Certificate]
+}>()
+
+const tableHeaders = [
+  {
+    label: t('standalone.certificates.name'),
+    key: 'name'
+  },
+  {
+    label: t('standalone.certificates.domains'),
+    key: 'domains'
+  },
+  {
+    label: t('standalone.certificates.expire'),
+    key: 'expire'
+  },
+  {
+    label: '',
+    key: 'menu'
+  }
+]
+
+function getDropdownItems(item: Certificate) {
+  return [
+    {
+      id: 'set_as_default',
+      label: t('standalone.certificates.set_as_default'),
+      iconStyle: 'fas',
+      icon: 'circle-check',
+      action: () => {
+        emit('setAsDefault', item)
+      }
+    },
+    {
+      id: 'delete',
+      label: t('common.delete'),
+      iconStyle: 'fas',
+      icon: 'trash',
+      danger: true,
+      action: () => {
+        emit('delete', item)
+      }
+    }
+  ]
+}
+</script>
+
+<template>
+  <NeTable :data="certificates" :headers="tableHeaders" class="z-10">
+    <template #name="{ item }: { item: Certificate }">
+      <div class="flex flex-row items-center">
+        <div class="mr-6">
+          <p>{{ item.path }}</p>
+          <NeButton class="-mx-2" kind="tertiary" @click="emit('showCertificate', item)">{{
+            t('standalone.certificates.more_info')
+          }}</NeButton>
+        </div>
+        <NeBadge
+          v-if="item.default"
+          kind="success"
+          class="-mt-2"
+          :text="t('standalone.certificates.default')"
+        />
+      </div>
+    </template>
+    <template #domains="{ item }: { item: Certificate }">
+      <p>{{ item.domains.join(', ') }}</p>
+    </template>
+    <template #expire="{ item }: { item: Certificate }">
+      <div class="flex flex-row gap-x-2">
+        <p>
+          {{
+            `${new Date(item.expiration * 1000).toLocaleDateString()} ${new Date(
+              item.expiration * 1000
+            ).toLocaleTimeString()}`
+          }}
+        </p>
+        <!-- certificate expired warning -->
+        <NeTooltip interactive v-if="item.expired">
+          <template #trigger>
+            <font-awesome-icon
+              :icon="['fas', 'triangle-exclamation']"
+              class="h-4 w-4 text-amber-500"
+            />
+          </template>
+          <template #content>
+            <p class="text-center">{{ t('standalone.certificates.certificate_expired') }}</p>
+          </template>
+        </NeTooltip>
+      </div>
+    </template>
+    <template #menu="{ item }: { item: Certificate }">
+      <div class="align-center flex justify-end">
+        <NeButton kind="tertiary" @click="emit('edit', item)">
+          <template #prefix>
+            <font-awesome-icon
+              :icon="['fas', 'pen-to-square']"
+              class="h-4 w-4"
+              aria-hidden="true"
+            />
+          </template>
+          {{ t('common.edit') }}
+        </NeButton>
+        <NeDropdown :items="getDropdownItems(item)" :align-to-right="true" />
+      </div>
+    </template>
+  </NeTable>
+</template>
--- src/views/standalone/system/CertificatesView.vue
+++ src/views/standalone/system/CertificatesView.vue
@@ -1,10 +1,124 @@
+<!--
+  Copyright (C) 2023 Nethesis S.r.l.
+  SPDX-License-Identifier: GPL-3.0-or-later
+-->
+
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
-import { NeTitle } from '@nethserver/vue-tailwind-lib'
+import {
+  NeTitle,
+  NeButton,
+  getAxiosErrorMessage,
+  NeSkeleton,
+  NeInlineNotification
+} from '@nethserver/vue-tailwind-lib'
+import { ref } from 'vue'
+import { ubusCall } from '@/lib/standalone/ubus'
+import CertificatesTable from '@/components/standalone/certificates/CertificatesTable.vue'
+
+export type Certificate = {
+  type: string
+  path: string
+  default: boolean
+  domains: string[]
+  expired: boolean
+  expiration: number
+}
 
 const { t } = useI18n()
+
+const certificates = ref<Certificate[]>([
+  {
+    type: 'acme',
+    path: '/etc/nginx/conf.d/test.crt',
+    default: true,
+    domains: ['a.b.c', 'd.e.f'],
+    expired: false,
+    expiration: 1705762469
+  }
+])
+const error = ref({
+  notificationTitle: '',
+  notificationDescription: '',
+  notificationDetails: ''
+})
+const loading = ref(false)
+const fetchError = ref(false)
+const selectedCertificate = ref<Certificate>()
+const showDeleteCertificateModal = ref(false)
+const showCreateOrEditCertificateDrawer = ref(false)
+
+function openCreateEditCertificateDrawer(itemToEdit?: Certificate) {
+  selectedCertificate.value = itemToEdit
+  showCreateOrEditCertificateDrawer.value = true
+}
+
+function openDeleteCertificateModal(itemToDelete: Certificate) {
+  selectedCertificate.value = itemToDelete
+  showDeleteCertificateModal.value = true
+}
+
+async function fetchCertificates() {
+  error.value.notificationTitle = ''
+  error.value.notificationDescription = ''
+  error.value.notificationDetails = ''
+
+  try {
+    loading.value = true
+    certificates.value = (await ubusCall('ns.reverseproxy', 'list-certificates')).data.rules
+  } catch (err: any) {
+    error.value.notificationTitle = t('error.cannot_retrieve_certificates')
+    error.value.notificationDescription = t(getAxiosErrorMessage(err))
+    error.value.notificationDetails = err.toString()
+    fetchError.value = true
+  } finally {
+    loading.value = false
+  }
+}
 </script>
 
 <template>
   <NeTitle>{{ t('standalone.certificates.title') }}</NeTitle>
+  <div class="flex flex-col gap-y-6">
+    <NeInlineNotification
+      v-if="error.notificationDescription"
+      :title="error.notificationTitle"
+      :description="error.notificationDescription"
+      class="mb-6"
+      kind="error"
+    >
+      <template #details v-if="error.notificationDetails">
+        {{ error.notificationDetails }}
+      </template></NeInlineNotification
+    >
+    <NeSkeleton v-if="loading" :lines="15" />
+    <template v-else-if="!fetchError">
+      <div class="flex flex-row items-center justify-between">
+        <p class="max-w-2xl text-sm font-normal text-gray-500 dark:text-gray-400">
+          {{ t('standalone.certificates.description') }}
+        </p>
+        <div class="flex flex-row">
+          <NeButton class="mr-2" kind="tertiary" @click="() => {}"
+            ><template #prefix>
+              <font-awesome-icon
+                :icon="['fas', 'circle-arrow-up']"
+                class="h-4 w-4"
+                aria-hidden="true"
+              /> </template
+            >{{ t('standalone.certificates.import_certificate') }}</NeButton
+          >
+          <NeButton kind="secondary" @click="() => {}"
+            ><template #prefix>
+              <font-awesome-icon
+                :icon="['fas', 'circle-plus']"
+                class="h-4 w-4"
+                aria-hidden="true"
+              /> </template
+            >{{ t('standalone.certificates.add_lets_encrypt_certificate') }}</NeButton
+          >
+        </div>
+      </div>
+      <CertificatesTable :certificates="certificates" />
+    </template>
+  </div>
 </template>

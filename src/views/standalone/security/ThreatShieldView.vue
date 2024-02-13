13c5f30ad45905464f2cccad53ac9f82aa3c3bab
Kevin Cela <kevin.cela@aidaptsrl.com>
2024-02-13 17:22:46 +0000

    feat(threat-shield): add tabs skeleton and settings tab

--- src/views/standalone/security/ThreatShieldView.vue
+++ src/views/standalone/security/ThreatShieldView.vue
@@ -6,10 +6,43 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
 import { NeTitle } from '@nethesis/vue-components'
+import { NeTabs } from '@nethserver/vue-tailwind-lib'
+import { useTabs } from '@/composables/useTabs'
+import AllowlistTab from '@/components/standalone/security/threat_shield/AllowlistTab.vue'
+import BlocklistTab from '@/components/standalone/security/threat_shield/BlocklistTab.vue'
+import SettingsTab from '@/components/standalone/security/threat_shield/SettingsTab.vue'
 
 const { t } = useI18n()
+
+const { tabs, selectedTab } = useTabs([
+  {
+    name: 'blocklist',
+    label: t('standalone.threat_shield.blocklist')
+  },
+  {
+    name: 'allowlist',
+    label: t('standalone.threat_shield.allowlist')
+  },
+  {
+    name: 'settings',
+    label: t('standalone.threat_shield.settings')
+  }
+])
 </script>
 
 <template>
   <NeTitle>{{ t('standalone.threat_shield.title') }}</NeTitle>
+  <div>
+    <NeTabs
+      :selected="selectedTab"
+      :srSelectTabLabel="t('ne_tabs.select_a_tab')"
+      :srTabsLabel="t('ne_tabs.tabs')"
+      :tabs="tabs"
+      class="mb-8"
+      @selectTab="selectedTab = $event"
+    />
+    <BlocklistTab v-if="selectedTab === 'blocklist'" />
+    <AllowlistTab v-if="selectedTab === 'allowlist'" />
+    <SettingsTab v-if="selectedTab === 'settings'" />
+  </div>
 </template>

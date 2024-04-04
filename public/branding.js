790756623a83eeb41d6b2c98b348af4358cc67a3
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2024-04-04 14:28:44 +0200

    fix: branding, change product and docs URLs (#233)

--- public/branding.js
+++ public/branding.js
@@ -1,7 +1,7 @@
 window.BRANDING = {
   PRODUCT_NAME: 'NethSecurity',
-  PRODUCT_URL: 'https://www.nethesis.it/',
-  DOCS_URL: 'https://nethserver.github.io/nethsecurity/',
+  PRODUCT_URL: 'https://www.nethsecurity.org/',
+  DOCS_URL: 'https://docs.nethsecurity.org/',
   COMPANY_NAME: 'Nethesis',
   COMPANY_URL: 'https://www.nethesis.it/',
   PRIVACY_POLICY_URL: ''

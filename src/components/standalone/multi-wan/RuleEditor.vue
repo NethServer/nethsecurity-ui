0246f95123e6f3d778df436caa5d29fc166aec9a
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2024-07-29 16:24:40 +0200

    feat(mwan): adding support for objects

--- src/components/standalone/multi-wan/RuleEditor.vue
+++ src/components/standalone/multi-wan/RuleEditor.vue
@@ -12,7 +12,8 @@ import {
   NeSideDrawer,
   NeTextInput,
   NeTooltip,
-  NeToggle
+  NeToggle,
+  NeRadioSelection
 } from '@nethesis/vue-components'
 import { MessageBag } from '@/lib/validation'
 import type { Policy, Rule } from '@/composables/useMwan'
@@ -39,7 +40,15 @@ const {
   destinationPort,
   sticky,
   validationErrors,
-  isValid
+  isValid,
+  addressOptions,
+  srcType,
+  dstType,
+  objectsLoading,
+  srcObject,
+  srcObjectOptions,
+  dstObject,
+  dstObjectOptions
 } = useRuleForm(
   toRef(() => props.policies),
   toRef(() => props.rule)
@@ -63,22 +72,60 @@ function cleanForm() {
   sourcePort.value = ''
   destinationAddress.value = ''
   destinationPort.value = ''
+  srcObject.value = ''
+  dstObject.value = ''
   validationErrors.value.clear()
 }
+
+type Payload = {
+  name: string | undefined
+  label: string
+  policy: string
+  protocol: string
+  source_address?: string
+  source_port: string
+  destination_address?: string
+  destination_port: string
+  sticky: boolean
+  ns_src?: string
+  ns_dst?: string
+}
+
 function save() {
   if (isValid()) {
     saving.value = true
-    ubusCall('ns.mwan', 'edit_rule', {
+    const payload: Payload = {
       name: props.rule?.name,
       label: name.value,
       policy: policy.value,
       protocol: protocol.value,
-      source_address: sourceAddress.value,
       source_port: sourcePort.value,
-      destination_address: destinationAddress.value,
       destination_port: destinationPort.value,
       sticky: sticky.value
-    })
+    }
+    switch (srcType.value) {
+      case 'address':
+        payload.source_address = sourceAddress.value
+        break
+      case 'object':
+        payload.ns_src = srcObject.value
+        break
+      default:
+        payload.source_address = ''
+        break
+    }
+    switch (dstType.value) {
+      case 'address':
+        payload.destination_address = destinationAddress.value
+        break
+      case 'object':
+        payload.ns_dst = dstObject.value
+        break
+      default:
+        payload.destination_address = ''
+        break
+    }
+    ubusCall('ns.mwan', 'edit_rule', payload)
       .then(() => emit('success'))
       .catch((reason: Error) => {
         if (reason instanceof ValidationError) {
@@ -137,13 +184,35 @@ function save() {
         :user-input-label="t('ne_combobox.user_input_label')"
         :optionalLabel="t('common.optional')"
       />
+      <NeRadioSelection
+        v-model="srcType"
+        :disabled="saving"
+        :label="t('standalone.multi_wan.source_type')"
+        :options="addressOptions"
+      />
       <NeTextInput
+        v-if="srcType == 'address'"
         v-model.trim="sourceAddress"
         :disabled="saving"
         :invalid-message="t(validationErrors.getFirstI18nKeyFor('source_address'))"
         :label="t('standalone.multi_wan.source_address')"
         name="source_address"
         placeholder="any"
+      />
+      <NeCombobox
+        v-if="srcType == 'object'"
+        v-model="srcObject"
+        :disabled="saving"
+        :label="t('standalone.multi_wan.source_object')"
+        :options="srcObjectOptions"
+        :placeholder="objectsLoading ? t('common.loading') : t('ne_combobox.choose')"
+        :invalid-message="t(validationErrors.getFirstI18nKeyFor('ns_src'))"
+        :optionalLabel="t('common.optional')"
+        :noResultsLabel="t('ne_combobox.no_results')"
+        :limitedOptionsLabel="t('ne_combobox.limited_options_label')"
+        :noOptionsLabel="t('ne_combobox.no_options_label')"
+        :selected-label="t('ne_combobox.selected')"
+        :user-input-label="t('ne_combobox.user_input_label')"
       />
       <NeTextInput
         v-if="protocol == 'tcp' || protocol == 'udp'"
@@ -162,13 +231,35 @@ function save() {
           </NeTooltip>
         </template>
       </NeTextInput>
+      <NeRadioSelection
+        v-model="dstType"
+        :disabled="saving"
+        :label="t('standalone.multi_wan.destination_type')"
+        :options="addressOptions"
+      />
       <NeTextInput
+        v-if="dstType == 'address'"
         v-model.trim="destinationAddress"
         :disabled="saving"
         :invalid-message="t(validationErrors.getFirstI18nKeyFor('destination_address'))"
         :label="t('standalone.multi_wan.destination_address')"
         :placeholder="t('standalone.multi_wan.any')"
         name="destination_address"
+      />
+      <NeCombobox
+        v-if="dstType == 'object'"
+        v-model="dstObject"
+        :disabled="saving"
+        :label="t('standalone.multi_wan.destination_object')"
+        :options="dstObjectOptions"
+        :placeholder="objectsLoading ? t('common.loading') : t('ne_combobox.choose')"
+        :invalid-message="t(validationErrors.getFirstI18nKeyFor('ns_dst'))"
+        :optionalLabel="t('common.optional')"
+        :noResultsLabel="t('ne_combobox.no_results')"
+        :limitedOptionsLabel="t('ne_combobox.limited_options_label')"
+        :noOptionsLabel="t('ne_combobox.no_options_label')"
+        :selected-label="t('ne_combobox.selected')"
+        :user-input-label="t('ne_combobox.user_input_label')"
       />
       <NeTextInput
         v-if="protocol == 'tcp' || protocol == 'udp'"

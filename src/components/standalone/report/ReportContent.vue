b1d0c02cb954afb819a6956ade15d54d12b2f0d4
Andrea Leardini <andre8244@gmail.com>
2024-08-27 16:53:24 +0200

    fix: disable "Open report" button from controlled unit

--- src/components/standalone/report/ReportContent.vue
+++ src/components/standalone/report/ReportContent.vue
@@ -5,9 +5,10 @@
 
 <script lang="ts" setup>
 import { useI18n } from 'vue-i18n'
-import { NeButton } from '@nethesis/vue-components'
+import { NeButton, NeTooltip } from '@nethesis/vue-components'
 import FormLayout from '@/components/standalone/FormLayout.vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { isStandaloneMode } from '@/lib/config'
 
 const { t } = useI18n()
 
@@ -23,12 +24,26 @@ function goToReport() {
       class="max-w-12xl"
     >
       <div class="mr-auto self-start">
-        <NeButton class="mr-2" kind="secondary" size="lg" @click="goToReport()">
+        <NeButton v-if="isStandaloneMode()" kind="secondary" size="lg" @click="goToReport()">
           <template #prefix>
             <FontAwesomeIcon :icon="['fa', 'arrow-up-right-from-square']" />
           </template>
           {{ t('standalone.report.real_time_report.open_report') }}
         </NeButton>
+        <!-- cannot open report from a controlled unit -->
+        <NeTooltip v-else triggerEvent="mouseenter focus">
+          <template #trigger>
+            <NeButton kind="secondary" size="lg" disabled>
+              <template #prefix>
+                <FontAwesomeIcon :icon="['fa', 'arrow-up-right-from-square']" />
+              </template>
+              {{ t('standalone.report.real_time_report.open_report') }}
+            </NeButton>
+          </template>
+          <template #content>
+            {{ t('standalone.report.real_time_report.cannot_open_report_from_controller') }}
+          </template>
+        </NeTooltip>
       </div>
     </FormLayout>
   </div>

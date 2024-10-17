6cfff58061b5c5a0aa6bdc3f17c733299bbc0a29
Andrea Leardini <andrea.leardini@nethesis.it>
2024-10-17 17:29:17 +0200

    refactor: import new NeTable (#405)
    * fix: import NeTable from vue-components
    
    Use improved NeTable for certificates table
    
    * fix: import NeTable from vue-components
    
    Use improved NeTable for NAT rules table

--- src/components/standalone/firewall/nat/NatRulesTable.vue
+++ src/components/standalone/firewall/nat/NatRulesTable.vue
@@ -6,13 +6,23 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
 import { NeButton } from '@nethesis/vue-components'
-import { NeBadge, NeDropdown } from '@nethesis/vue-components'
-import { type PropType } from 'vue'
-import NeTable from '@/components/standalone/NeTable.vue'
+import {
+  NeBadge,
+  NeDropdown,
+  NeTable,
+  NeTableHead,
+  NeTableHeadCell,
+  NeTableBody,
+  NeTableRow,
+  NeTableCell,
+  NePaginator,
+  useItemPagination
+} from '@nethesis/vue-components'
+import { ref, type PropType } from 'vue'
 import { type NatRule } from '@/stores/standalone/firewall'
 import { getZoneColorClasses } from '@/lib/standalone/network'
 
-defineProps({
+const props = defineProps({
   rules: {
     type: Array as PropType<NatRule[]>,
     required: true
@@ -26,37 +36,10 @@ defineProps({
 const emit = defineEmits(['reloadRules', 'editRule', 'deleteRule'])
 
 const { t } = useI18n()
-
-const headers = [
-  {
-    label: t('standalone.nat.name'),
-    key: 'name'
-  },
-  {
-    label: t('standalone.nat.source_address'),
-    key: 'src_ip'
-  },
-  {
-    label: t('standalone.nat.outbound_zone'),
-    key: 'src'
-  },
-  {
-    label: t('standalone.nat.destination_address'),
-    key: 'dest_ip'
-  },
-  {
-    label: t('standalone.nat.action'),
-    key: 'target'
-  },
-  {
-    label: t('standalone.nat.rewrite_ip'),
-    key: 'snat_ip'
-  },
-  {
-    label: '',
-    key: 'menu'
-  }
-]
+const pageSize = ref(10)
+const { currentPage, paginatedItems } = useItemPagination(() => props.rules, {
+  itemsPerPage: pageSize
+})
 
 function getDropdownItems(rule: NatRule) {
   return [
@@ -74,37 +57,87 @@ function getDropdownItems(rule: NatRule) {
 
 <template>
   <div>
-    <NeTable :data="rules" :headers="headers" :loading="loading">
-      <template #src="{ item }: { item: NatRule }">
-        <span v-if="item.src === '*'">{{ t('common.any') }}</span>
-        <NeBadge
-          v-else
-          :text="item.src.toUpperCase()"
-          kind="custom"
-          :customColorClasses="getZoneColorClasses(item.src)"
+    <NeTable
+      :ariaLabel="t('standalone.nat.nat_rules')"
+      cardBreakpoint="xl"
+      :loading="loading"
+      :skeletonColumns="6"
+      :skeletonRows="3"
+    >
+      <NeTableHead>
+        <NeTableHeadCell>{{ t('standalone.nat.name') }}</NeTableHeadCell>
+        <NeTableHeadCell>{{ t('standalone.nat.source_address') }}</NeTableHeadCell>
+        <NeTableHeadCell>{{ t('standalone.nat.outbound_zone') }}</NeTableHeadCell>
+        <NeTableHeadCell>{{ t('standalone.nat.destination_address') }}</NeTableHeadCell>
+        <NeTableHeadCell>{{ t('standalone.nat.action') }}</NeTableHeadCell>
+        <NeTableHeadCell>{{ t('standalone.nat.rewrite_ip') }}</NeTableHeadCell>
+        <NeTableHeadCell>
+          <!-- no header for actions -->
+        </NeTableHeadCell>
+      </NeTableHead>
+      <NeTableBody>
+        <NeTableRow v-for="(item, index) in paginatedItems" :key="index">
+          <NeTableCell :data-label="t('standalone.nat.name')">
+            {{ item.name }}
+          </NeTableCell>
+          <NeTableCell :data-label="t('standalone.nat.source_address')">
+            {{ item.src_ip }}
+          </NeTableCell>
+          <NeTableCell :data-label="t('standalone.nat.outbound_zone')">
+            <span v-if="item.src === '*'">{{ t('common.any') }}</span>
+            <NeBadge
+              v-else
+              :text="item.src.toUpperCase()"
+              kind="custom"
+              :customColorClasses="getZoneColorClasses(item.src)"
+            />
+          </NeTableCell>
+          <NeTableCell :data-label="t('standalone.nat.destination_address')">
+            {{ item.dest_ip }}
+          </NeTableCell>
+          <NeTableCell :data-label="t('standalone.nat.action')">
+            {{ item.target }}
+          </NeTableCell>
+          <NeTableCell :data-label="t('standalone.nat.rewrite_ip')">
+            <span v-if="item.snat_ip">{{ item.snat_ip }}</span>
+            <span v-else> - </span>
+          </NeTableCell>
+          <NeTableCell :data-label="t('common.actions')">
+            <div class="align-center -ml-2.5 flex items-center gap-2 xl:ml-0 xl:justify-end">
+              <NeButton kind="tertiary" size="lg" @click="emit('editRule', item)">
+                <template #prefix>
+                  <font-awesome-icon
+                    :icon="['fas', 'pen-to-square']"
+                    class="h-4 w-4"
+                    aria-hidden="true"
+                  />
+                </template>
+                {{ t('common.edit') }}
+              </NeButton>
+              <NeDropdown :items="getDropdownItems(item)" :align-to-right="true" />
+            </div>
+          </NeTableCell>
+        </NeTableRow>
+      </NeTableBody>
+      <template #paginator>
+        <NePaginator
+          :current-page="currentPage"
+          :total-rows="props.rules.length"
+          :page-size="pageSize"
+          :nav-pagination-label="t('ne_table.pagination')"
+          :next-label="t('ne_table.go_to_next_page')"
+          :previous-label="t('ne_table.go_to_previous_page')"
+          :range-of-total-label="t('ne_table.of')"
+          :page-size-label="t('ne_table.show')"
+          @select-page="
+            (page: number) => {
+              currentPage = page
+            }"
+          @selectPageSize="
+            (size: number) => {
+              pageSize = size
+            }"
         />
-      </template>
-      <template #target="{ item }: { item: NatRule }">
-        {{ item.target }}
-      </template>
-      <template #snat_ip="{ item }: { item: NatRule }">
-        <span v-if="item.snat_ip">{{ item.snat_ip }}</span>
-        <span v-else> - </span>
-      </template>
-      <template #menu="{ item }: { item: NatRule }">
-        <div class="align-center flex justify-end">
-          <NeButton kind="tertiary" size="lg" @click="emit('editRule', item)" class="mr-2">
-            <template #prefix>
-              <font-awesome-icon
-                :icon="['fas', 'pen-to-square']"
-                class="h-4 w-4"
-                aria-hidden="true"
-              />
-            </template>
-            {{ t('common.edit') }}
-          </NeButton>
-          <NeDropdown :items="getDropdownItems(item)" :align-to-right="true" />
-        </div>
       </template>
     </NeTable>
   </div>

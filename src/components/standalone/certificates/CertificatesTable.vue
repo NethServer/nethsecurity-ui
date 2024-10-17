5f8dd6ff69f0c8a3d45eaf846a75883dd956503e
Andrea Leardini <andre8244@gmail.com>
2024-10-17 11:38:22 +0200

    fix: import NeTable from vue-components
    Use improved NeTable for certificates table

--- src/components/standalone/certificates/CertificatesTable.vue
+++ src/components/standalone/certificates/CertificatesTable.vue
@@ -5,15 +5,33 @@
 
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
-import NeTable from '../NeTable.vue'
-import { NeDropdown, NeButton, NeBadge, NeTooltip } from '@nethesis/vue-components'
+import {
+  NeDropdown,
+  NeBadge,
+  NeTable,
+  NeTableHead,
+  NeTableHeadCell,
+  NeTableBody,
+  NeTableRow,
+  NeTableCell,
+  NePaginator,
+  useItemPagination,
+  NeButton,
+  NeTooltip
+} from '@nethesis/vue-components'
 import type { Certificate } from '@/views/standalone/system/CertificatesView.vue'
+import { ref } from 'vue'
 
 const { t } = useI18n()
 
-defineProps<{
+const props = defineProps<{
   certificates: Certificate[]
 }>()
+
+const pageSize = ref(10)
+const { currentPage, paginatedItems } = useItemPagination(() => props.certificates, {
+  itemsPerPage: pageSize
+})
 
 const emit = defineEmits<{
   delete: [item: Certificate]
@@ -21,29 +39,6 @@ const emit = defineEmits<{
   setAsDefault: [item: Certificate]
   showCertificate: [item: Certificate]
 }>()
-
-const tableHeaders = [
-  {
-    label: t('standalone.certificates.name'),
-    key: 'name'
-  },
-  {
-    label: t('standalone.certificates.domains'),
-    key: 'domains'
-  },
-  {
-    label: t('standalone.certificates.type'),
-    key: 'type'
-  },
-  {
-    label: t('standalone.certificates.expire'),
-    key: 'expire'
-  },
-  {
-    label: '',
-    key: 'menu'
-  }
-]
 
 function isCertificateExpired(item: Certificate) {
   return item.expiration ? new Date(Date.parse(item.expiration)).getTime() < Date.now() : false
@@ -106,87 +101,130 @@ function getDropdownItems(item: Certificate) {
 </script>
 
 <template>
-  <NeTable :data="certificates" :headers="tableHeaders" class="z-10">
-    <template #name="{ item }: { item: Certificate }">
-      <div class="flex flex-row items-center">
-        <div class="mr-6">
-          <p>{{ item.name }}</p>
-          <NeButton
-            :disabled="!item.details"
-            class="-mx-2"
-            kind="tertiary"
-            @click="emit('showCertificate', item)"
-            >{{ t('standalone.certificates.more_info') }}</NeButton
-          >
-        </div>
-        <!-- default certificate badge and tooltip -->
-        <NeTooltip interactive v-if="item.default">
-          <template #trigger>
-            <NeBadge kind="success" class="-mt-2" :text="t('standalone.certificates.default')" />
+  <NeTable :ariaLabel="t('standalone.certificates.title')" cardBreakpoint="lg" class="z-10">
+    <NeTableHead>
+      <NeTableHeadCell>{{ t('standalone.certificates.name') }}</NeTableHeadCell>
+      <NeTableHeadCell>{{ t('standalone.certificates.domains') }}</NeTableHeadCell>
+      <NeTableHeadCell>{{ t('standalone.certificates.type') }}</NeTableHeadCell>
+      <NeTableHeadCell>{{ t('standalone.certificates.expire') }}</NeTableHeadCell>
+      <NeTableHeadCell>
+        <!-- no header for actions -->
+      </NeTableHeadCell>
+    </NeTableHead>
+    <NeTableBody>
+      <NeTableRow v-for="(item, index) in paginatedItems" :key="index">
+        <NeTableCell :data-label="t('standalone.certificates.name')">
+          <div class="flex flex-row items-center">
+            <div class="mr-6">
+              <p>{{ item.name }}</p>
+              <NeButton
+                :disabled="!item.details"
+                class="-mx-2"
+                kind="tertiary"
+                @click="emit('showCertificate', item)"
+                >{{ t('standalone.certificates.more_info') }}</NeButton
+              >
+            </div>
+            <!-- default certificate badge and tooltip -->
+            <NeTooltip interactive v-if="item.default">
+              <template #trigger>
+                <NeBadge
+                  kind="success"
+                  class="-mt-2"
+                  :text="t('standalone.certificates.default')"
+                />
+              </template>
+              <template #content>
+                <p class="text-center">
+                  {{ t('standalone.certificates.default_certificate_tooltip') }}
+                </p>
+              </template></NeTooltip
+            >
+          </div>
+        </NeTableCell>
+        <NeTableCell :data-label="t('standalone.certificates.domains')">
+          <p v-if="item.type != 'acme'">{{ item.domain ? item.domain : '-' }}</p>
+          <template v-else>
+            <p>{{ item.requested_domains!.slice(0, 2).join(', ') }}</p>
+            <NeTooltip interactive placement="bottom" v-if="item.requested_domains!.length > 2">
+              <template #trigger>
+                <NeButton size="sm" kind="tertiary" class="-mx-2">{{
+                  t('standalone.certificates.plus_n_others', {
+                    n: item.requested_domains!.length - 2
+                  })
+                }}</NeButton>
+              </template>
+              <template #content>
+                <p>{{ item.requested_domains!.slice(2).join(', ') }}</p>
+              </template>
+            </NeTooltip>
           </template>
-          <template #content>
-            <p class="text-center">
-              {{ t('standalone.certificates.default_certificate_tooltip') }}
+        </NeTableCell>
+        <NeTableCell :data-label="t('standalone.certificates.type')">
+          <div class="flex flex-row items-center">
+            <p class="mr-6">{{ getCertificateType(item) }}</p>
+            <!-- pending badge and tooltip -->
+            <NeTooltip interactive v-if="item.pending">
+              <template #trigger>
+                <NeBadge kind="warning" :text="t('standalone.certificates.pending')" />
+              </template>
+              <template #content>
+                <p class="text-center">
+                  {{ t('standalone.certificates.pending_tooltip') }}
+                </p>
+              </template></NeTooltip
+            >
+          </div>
+        </NeTableCell>
+        <NeTableCell :data-label="t('standalone.certificates.expire')">
+          <div class="flex flex-row gap-x-2">
+            <p>
+              {{ getFormattedExpiration(item) }}
             </p>
-          </template></NeTooltip
-        >
-      </div>
-    </template>
-    <template #domains="{ item }: { item: Certificate }">
-      <p v-if="item.type != 'acme'">{{ item.domain ? item.domain : '-' }}</p>
-      <template v-else>
-        <p>{{ item.requested_domains!.slice(0, 2).join(', ') }}</p>
-        <NeTooltip interactive placement="bottom" v-if="item.requested_domains!.length > 2">
-          <template #trigger>
-            <NeButton size="sm" kind="tertiary" class="-mx-2">{{
-              t('standalone.certificates.plus_n_others', { n: item.requested_domains!.length - 2 })
-            }}</NeButton>
-          </template>
-          <template #content>
-            <p>{{ item.requested_domains!.slice(2).join(', ') }}</p>
-          </template>
-        </NeTooltip>
-      </template>
-    </template>
-    <template #type="{ item }: { item: Certificate }">
-      <div class="flex flex-row items-center">
-        <p class="mr-6">{{ getCertificateType(item) }}</p>
-        <!-- pending badge and tooltip -->
-        <NeTooltip interactive v-if="item.pending">
-          <template #trigger>
-            <NeBadge kind="warning" :text="t('standalone.certificates.pending')" />
-          </template>
-          <template #content>
-            <p class="text-center">
-              {{ t('standalone.certificates.pending_tooltip') }}
-            </p>
-          </template></NeTooltip
-        >
-      </div>
-    </template>
-    <template #expire="{ item }: { item: Certificate }">
-      <div class="flex flex-row gap-x-2">
-        <p>
-          {{ getFormattedExpiration(item) }}
-        </p>
-        <!-- certificate expired warning -->
-        <NeTooltip interactive v-if="isCertificateExpired(item)">
-          <template #trigger>
-            <font-awesome-icon
-              :icon="['fas', 'triangle-exclamation']"
-              class="h-4 w-4 text-amber-500"
+            <!-- certificate expired warning -->
+            <NeTooltip interactive v-if="isCertificateExpired(item)">
+              <template #trigger>
+                <font-awesome-icon
+                  :icon="['fas', 'triangle-exclamation']"
+                  class="h-4 w-4 text-amber-500"
+                />
+              </template>
+              <template #content>
+                <p class="text-center">{{ t('standalone.certificates.certificate_expired') }}</p>
+              </template>
+            </NeTooltip>
+          </div>
+        </NeTableCell>
+        <NeTableCell :data-label="t('common.actions')">
+          <div class="align-center flex justify-end">
+            <NeDropdown
+              v-if="!item.default"
+              :items="getDropdownItems(item)"
+              :align-to-right="true"
             />
-          </template>
-          <template #content>
-            <p class="text-center">{{ t('standalone.certificates.certificate_expired') }}</p>
-          </template>
-        </NeTooltip>
-      </div>
-    </template>
-    <template #menu="{ item }: { item: Certificate }">
-      <div class="align-center flex justify-end">
-        <NeDropdown v-if="!item.default" :items="getDropdownItems(item)" :align-to-right="true" />
-      </div>
+          </div>
+        </NeTableCell>
+      </NeTableRow>
+    </NeTableBody>
+    <template #paginator>
+      <NePaginator
+        :current-page="currentPage"
+        :total-rows="props.certificates.length"
+        :page-size="pageSize"
+        :nav-pagination-label="t('ne_table.pagination')"
+        :next-label="t('ne_table.go_to_next_page')"
+        :previous-label="t('ne_table.go_to_previous_page')"
+        :range-of-total-label="t('ne_table.of')"
+        :page-size-label="t('ne_table.show')"
+        @select-page="
+            (page: number) => {
+              currentPage = page
+            }"
+        @selectPageSize="
+            (size: number) => {
+              pageSize = size
+            }"
+      />
     </template>
   </NeTable>
 </template>

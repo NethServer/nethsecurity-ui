13409362a4bea8db48ba7863aae3ef3af1181fa4
Bailetti Tommaso <tommaso.bailetti@nethesis.it>
2025-01-13 17:47:14 +0100

    feat: added IPS view (#481)

--- src/components/standalone/SideMenu.vue
+++ src/components/standalone/SideMenu.vue
@@ -188,6 +188,10 @@ const navigation: Ref<MenuItem[]> = ref([
       {
         name: 'standalone.dpi.title',
         to: 'security/dpi'
+      },
+      {
+        name: 'standalone.ips.sidebar_title',
+        to: 'security/ips'
       }
     ],
     expanded: false

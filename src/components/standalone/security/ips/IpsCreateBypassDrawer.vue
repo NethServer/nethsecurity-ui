3ce2585dffd2814b61d1ec3841bc28d5638d3e88
Bailetti Tommaso <tommaso.bailetti@nethesis.it>
2025-01-13 17:47:14 +0100

    feat: added IPS view (#481)

---
+++ src/components/standalone/security/ips/IpsCreateBypassDrawer.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import { NeSideDrawer } from '@nethesis/vue-components'
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+
+const { visible = false } = defineProps<{
+  visible: boolean
+}>()
+
+defineEmits(['close'])
+</script>
+
+<template>
+  <NeSideDrawer
+    :is-shown="visible"
+    :title="t('standalone.ips.create_bypass')"
+    @close="$emit('close')"
+  >
+    Hello!
+  </NeSideDrawer>
+</template>

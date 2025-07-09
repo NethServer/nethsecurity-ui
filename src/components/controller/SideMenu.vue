2d73aaab730de338b90bf4f9a586a9f7389545ce
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2025-07-09 11:26:58 +0200

    fix(controller): sidebar, move settings to the end

--- src/components/controller/SideMenu.vue
+++ src/components/controller/SideMenu.vue
@@ -29,27 +29,27 @@ const navigation: Ref<MenuItem[]> = ref([
   },
   ...(controllerLogin.isAdmin
     ? [
-        {
-          name: 'controller.unit_groups.title',
-          to: 'unit-groups',
-          icon: 'network-wired'
-        }
-      ]
+      {
+        name: 'controller.unit_groups.title',
+        to: 'unit-groups',
+        icon: 'network-wired'
+      }
+    ]
+    : []),
+  ...(controllerLogin.isAdmin
+    ? [
+      {
+        name: 'controller.users.title',
+        to: 'users',
+        icon: 'user-group'
+      }
+    ]
     : []),
   {
     name: 'controller.account_settings.title',
     to: 'account',
     icon: 'gear'
   },
-  ...(controllerLogin.isAdmin
-    ? [
-        {
-          name: 'controller.users.title',
-          to: 'users',
-          icon: 'user-group'
-        }
-      ]
-    : [])
 ])
 
 function isCurrentRoute(itemPath: string) {
@@ -59,20 +59,14 @@ function isCurrentRoute(itemPath: string) {
 
 <template>
   <li v-for="item in navigation" :key="item.name">
-    <router-link
-      :to="`${getControllerRoutePrefix()}/${item.to}`"
-      :class="[
-        isCurrentRoute(item.to)
-          ? 'border-l-4 border-primary-700 bg-gray-100 text-gray-900 dark:border-primary-500 dark:bg-gray-800 dark:text-gray-50'
-          : 'text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-50',
-        'group flex cursor-pointer items-center gap-x-3 rounded-md px-3 py-2 text-sm leading-6 font-semibold hover:bg-gray-100 dark:hover:bg-gray-800'
-      ]"
-    >
-      <font-awesome-icon
-        :icon="[isCurrentRoute(item.to) ? 'fas' : 'fal', item.icon]"
-        class="h-6 w-8 shrink-0"
-        aria-hidden="true"
-      />
+    <router-link :to="`${getControllerRoutePrefix()}/${item.to}`" :class="[
+      isCurrentRoute(item.to)
+        ? 'border-l-4 border-primary-700 bg-gray-100 text-gray-900 dark:border-primary-500 dark:bg-gray-800 dark:text-gray-50'
+        : 'text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-50',
+      'group flex cursor-pointer items-center gap-x-3 rounded-md px-3 py-2 text-sm font-semibold leading-6 hover:bg-gray-100 dark:hover:bg-gray-800'
+    ]">
+      <font-awesome-icon :icon="[isCurrentRoute(item.to) ? 'fas' : 'fal', item.icon]" class="h-6 w-8 shrink-0"
+        aria-hidden="true" />
       {{ t(item.name) }}
     </router-link>
   </li>

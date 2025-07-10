d37a89a9804d81e9ceeca660a1a30570d9187ccc
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2025-07-10 12:53:07 +0200

    fix: updated controller info

--- src/components/controller/ControllerTopBar.vue
+++ src/components/controller/ControllerTopBar.vue
@@ -33,7 +33,7 @@ const shakeNotificationsIcon = ref(false)
 const topBarButtonsColorClasses =
   'text-gray-600 dark:text-gray-300 hover:text-gray-900 dark:hover:text-gray-50'
 
-const platformInfo = ref<Record<string, any> | null>(null)
+const platformInfo = ref<Record<string, string | number>>({})
 const showPlatformInfoModal = ref(false)
 const platformInfoError = ref<string | null>(null)
 

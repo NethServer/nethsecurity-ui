bd5c28df5baf3d38632de3040a10053a6813ff1d
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2025-08-21 11:22:36 +0200

    fix(deps): updated vuei18n

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
 import { fileURLToPath, URL } from 'node:url'
+import path from 'node:path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    tailwindcss(),
+    VueI18nPlugin({
+      include: [path.resolve(__dirname, './src/i18n/**')]
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

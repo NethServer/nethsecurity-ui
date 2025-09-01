9523d3d1c89dd4789026c481345f0191d33e9e39
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2025-09-01 12:53:59 +0200

    fix: using browser internatiolization for names

--- src/components/ChangeLangCombobox.vue
+++ src/components/ChangeLangCombobox.vue
@@ -5,15 +5,19 @@ import { computed, watch } from 'vue'
 import { isStandaloneMode } from '@/lib/config'
 import { useLoginStore as useStandaloneLoginStore } from '@/stores/standalone/standaloneLogin'
 import { useLoginStore as useControllerLoginStore } from '@/stores/controller/controllerLogin'
+import { capitalize } from 'lodash-es'
 
 const { t, locale, availableLocales } = useI18n({ useScope: 'global' })
 const loginStore = isStandaloneMode() ? useStandaloneLoginStore() : useControllerLoginStore()
 
 const supportedLanguages = computed((): NeComboboxOption[] => {
   return availableLocales.map((locale) => {
+    const intl = new Intl.DisplayNames([locale], { type: 'language', fallback: 'code' })
+    // translated is always found and a string due to the fallback option: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DisplayNames/DisplayNames#fallback
+    const translated = intl.of(locale)!
     return {
       id: locale,
-      label: t(`languages.${locale}`)
+      label: capitalize(translated)
     }
   })
 })

f48e779035d48c6309c27b6ee7fa5aabb9c7ab86
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2025-09-18 10:01:11 +0200

    refactored server list in own component

---
+++ src/components/standalone/wireguard/WireguardServerTunnelList.vue
@@ -0,0 +1,170 @@
+<script lang="ts" setup>
+import {
+  NeButton,
+  NeCard,
+  NeEmptyState,
+  NeInlineNotification,
+  NeTextInput
+} from '@nethesis/vue-components'
+import { onMounted, ref } from 'vue'
+import type { AxiosResponse } from 'axios'
+import { ubusCall } from '@/lib/standalone/ubus.ts'
+import { faCirclePlus, faGlobe } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import WireguardTunnelDetailCard from '@/components/standalone/wireguard/WireguardTunnelDetailCard.vue'
+import TunnelDrawer from '@/components/standalone/wireguard/TunnelDrawer.vue'
+import DeletePeerModal from '@/components/standalone/wireguard/DeletePeerModal.vue'
+import DeleteTunnelModal from '@/components/standalone/wireguard/DeleteTunnelModal.vue'
+import PeerDrawer from '@/components/standalone/wireguard/PeerDrawer.vue'
+import { useUciPendingChangesStore } from '@/stores/standalone/uciPendingChanges.ts'
+import type { Peer, Tunnel } from '@/views/standalone/vpn/WireguardTunnelView.vue'
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+const changes = useUciPendingChangesStore()
+
+type ListInstancesResponse = {
+  instances: Tunnel[]
+}
+
+const loading = ref(true)
+const error = ref<Error>()
+const instances = ref<Tunnel[]>([])
+
+async function fetchData(): Promise<AxiosResponse<ListInstancesResponse>> {
+  error.value = undefined
+  return ubusCall<AxiosResponse<ListInstancesResponse>>('ns.wireguard', 'list-instances')
+    .then((response) => {
+      instances.value = response.data.instances
+    })
+    .catch((err) => (error.value = err))
+    .finally(() => (loading.value = false))
+}
+
+onMounted(() => fetchData())
+
+function refreshData(): Promise<unknown> {
+  return Promise.all([changes.getChanges(), fetchData()])
+}
+
+const showTunnelDrawer = ref(false)
+function createdTunnel() {
+  refreshData().finally(() => closeTunnelDrawer())
+}
+
+const editingTunnel = ref<Tunnel>()
+function editTunnel(instance: Tunnel) {
+  editingTunnel.value = instance
+  showTunnelDrawer.value = true
+}
+
+function closeTunnelDrawer() {
+  showTunnelDrawer.value = false
+  editingTunnel.value = undefined
+}
+
+const deletingTunnel = ref<Tunnel>()
+function deleteTunnel(instance: Tunnel) {
+  deletingTunnel.value = instance
+}
+function closeDeleteModal() {
+  deletingTunnel.value = undefined
+}
+function tunnelDeleted() {
+  refreshData().then(closeDeleteModal)
+}
+
+const showPeerDrawer = ref(false)
+const creatingPeerFor = ref<Tunnel>()
+const editPeer = ref<Peer>()
+function closePeerDrawer() {
+  showPeerDrawer.value = false
+  creatingPeerFor.value = undefined
+  editPeer.value = undefined
+}
+function addPeerHandler(instance: Tunnel) {
+  showPeerDrawer.value = true
+  creatingPeerFor.value = instance
+}
+function addedPeerHandler() {
+  refreshData().then(closePeerDrawer)
+}
+function editPeerHandler(instance: Tunnel, peer: Peer) {
+  showPeerDrawer.value = true
+  creatingPeerFor.value = instance
+  editPeer.value = peer
+}
+const peerToDelete = ref<Peer>()
+function deletePeerHandler(peer: Peer) {
+  peerToDelete.value = peer
+}
+function closeDeletePeerModal() {
+  peerToDelete.value = undefined
+}
+function peerDeleted() {
+  refreshData().then(closeDeletePeerModal)
+}
+</script>
+
+<template>
+  <div>
+    <NeCard v-if="loading" loading />
+    <div v-else class="space-y-6">
+      <p>{{ t('standalone.wireguard_tunnel.description') }}</p>
+      <NeInlineNotification v-if="error != undefined" kind="error" :title="t('common.error')" />
+      <template v-else-if="instances.length > 0">
+        <div class="flex flex-wrap gap-4">
+          <NeTextInput is-search :placeholder="t('common.filter')" class="mr-auto" />
+          <NeButton kind="secondary" @click="showTunnelDrawer = true">
+            <template #prefix>
+              <FontAwesomeIcon :icon="faCirclePlus" aria-hidden="true" class="h-4 w-4" />
+            </template>
+            {{ t('standalone.wireguard_tunnel.add_server') }}
+          </NeButton>
+        </div>
+        <WireguardTunnelDetailCard
+          v-for="instance in instances"
+          :key="instance.id"
+          :instance="instance"
+          @edit="editTunnel"
+          @delete="deleteTunnel"
+          @add-peer="addPeerHandler"
+          @edit-peer="editPeerHandler"
+          @delete-peer="deletePeerHandler"
+        />
+      </template>
+      <NeEmptyState
+        v-else
+        :title="t('standalone.wireguard_tunnel.no_server_configured')"
+        :description="t('standalone.wireguard_tunnel.no_server_configured_description')"
+        :icon="faGlobe"
+      >
+        <NeButton kind="primary" @click="showTunnelDrawer = true">
+          <template #prefix>
+            <FontAwesomeIcon :icon="faCirclePlus" aria-hidden="true" class="h-4 w-4" />
+          </template>
+          {{ t('standalone.wireguard_tunnel.add_server') }}
+        </NeButton>
+      </NeEmptyState>
+    </div>
+  </div>
+  <TunnelDrawer
+    :is-shown="showTunnelDrawer"
+    :tunnel="editingTunnel"
+    @success="createdTunnel"
+    @close="closeTunnelDrawer"
+  />
+  <DeleteTunnelModal
+    :instance="deletingTunnel"
+    @success="tunnelDeleted"
+    @close="closeDeleteModal"
+  />
+  <PeerDrawer
+    :is-shown="showPeerDrawer"
+    :instance="creatingPeerFor"
+    :peer="editPeer"
+    @close="closePeerDrawer"
+    @success="addedPeerHandler"
+  />
+  <DeletePeerModal :peer="peerToDelete" @success="peerDeleted" @close="closeDeletePeerModal" />
+</template>
--- src/views/standalone/vpn/WireguardTunnelView.vue
+++ src/views/standalone/vpn/WireguardTunnelView.vue
@@ -1,29 +1,10 @@
 <script setup lang="ts">
-import {
-  NeHeading,
-  NeCard,
-  NeTabs,
-  NeEmptyState,
-  NeButton,
-  NeTextInput,
-  NeInlineNotification
-} from '@nethesis/vue-components'
+import { NeHeading, NeTabs } from '@nethesis/vue-components'
 import { useI18n } from 'vue-i18n'
 import { useTabs } from '@/composables/useTabs.ts'
-import { ubusCall } from '@/lib/standalone/ubus.ts'
-import { onMounted, ref } from 'vue'
-import type { AxiosResponse } from 'axios'
-import WireguardTunnelDetailCard from '@/components/standalone/wireguard/WireguardTunnelDetailCard.vue'
-import { faCirclePlus, faGlobe } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import TunnelDrawer from '@/components/standalone/wireguard/TunnelDrawer.vue'
-import { useUciPendingChangesStore } from '@/stores/standalone/uciPendingChanges'
-import DeleteTunnelModal from '@/components/standalone/wireguard/DeleteTunnelModal.vue'
-import PeerDrawer from '@/components/standalone/wireguard/PeerDrawer.vue'
-import DeletePeerModal from '@/components/standalone/wireguard/DeletePeerModal.vue'
+import WireguardServerTunnelList from '@/components/standalone/wireguard/WireguardServerTunnelList.vue'
 
 const { t } = useI18n()
-const changes = useUciPendingChangesStore()
 
 const { tabs, selectedTab } = useTabs([
   {
@@ -63,88 +44,6 @@ export type Tunnel = {
   mtu: number
   dns: string
 }
-
-type ListInstancesResponse = {
-  instances: Tunnel[]
-}
-
-const loading = ref(true)
-const error = ref<Error>()
-const instances = ref<Tunnel[]>([])
-
-async function fetchData(): Promise<AxiosResponse<ListInstancesResponse>> {
-  error.value = undefined
-  return ubusCall<AxiosResponse<ListInstancesResponse>>('ns.wireguard', 'list-instances')
-    .then((response) => {
-      instances.value = response.data.instances
-    })
-    .catch((err) => (error.value = err))
-    .finally(() => (loading.value = false))
-}
-
-onMounted(() => fetchData())
-
-function refreshData(): Promise<unknown> {
-  return Promise.all([changes.getChanges(), fetchData()])
-}
-
-const showTunnelDrawer = ref(false)
-function createdTunnel() {
-  refreshData().finally(() => closeTunnelDrawer())
-}
-
-const editingTunnel = ref<Tunnel>()
-function editTunnel(instance: Tunnel) {
-  editingTunnel.value = instance
-  showTunnelDrawer.value = true
-}
-
-function closeTunnelDrawer() {
-  showTunnelDrawer.value = false
-  editingTunnel.value = undefined
-}
-
-const deletingTunnel = ref<Tunnel>()
-function deleteTunnel(instance: Tunnel) {
-  deletingTunnel.value = instance
-}
-function closeDeleteModal() {
-  deletingTunnel.value = undefined
-}
-function tunnelDeleted() {
-  refreshData().then(closeDeleteModal)
-}
-
-const showPeerDrawer = ref(false)
-const creatingPeerFor = ref<Tunnel>()
-const editPeer = ref<Peer>()
-function closePeerDrawer() {
-  showPeerDrawer.value = false
-  creatingPeerFor.value = undefined
-  editPeer.value = undefined
-}
-function addPeerHandler(instance: Tunnel) {
-  showPeerDrawer.value = true
-  creatingPeerFor.value = instance
-}
-function addedPeerHandler() {
-  refreshData().then(closePeerDrawer)
-}
-function editPeerHandler(instance: Tunnel, peer: Peer) {
-  showPeerDrawer.value = true
-  creatingPeerFor.value = instance
-  editPeer.value = peer
-}
-const peerToDelete = ref<Peer>()
-function deletePeerHandler(peer: Peer) {
-  peerToDelete.value = peer
-}
-function closeDeletePeerModal() {
-  peerToDelete.value = undefined
-}
-function peerDeleted() {
-  refreshData().then(closeDeletePeerModal)
-}
 </script>
 
 <template>
@@ -157,65 +56,6 @@ function peerDeleted() {
       :tabs="tabs"
       @select-tab="selectedTab = $event"
     />
-    <div v-if="selectedTab == 'server'">
-      <NeCard v-if="loading" loading />
-      <div v-else class="space-y-6">
-        <p>{{ t('standalone.wireguard_tunnel.description') }}</p>
-        <NeInlineNotification v-if="error != undefined" kind="error" :title="t('common.error')" />
-        <template v-else-if="instances.length > 0">
-          <div class="flex flex-wrap gap-4">
-            <NeTextInput is-search :placeholder="t('common.filter')" class="mr-auto" />
-            <NeButton kind="secondary" @click="showTunnelDrawer = true">
-              <template #prefix>
-                <FontAwesomeIcon :icon="faCirclePlus" aria-hidden="true" class="h-4 w-4" />
-              </template>
-              {{ t('standalone.wireguard_tunnel.add_server') }}
-            </NeButton>
-          </div>
-          <WireguardTunnelDetailCard
-            v-for="instance in instances"
-            :key="instance.id"
-            :instance="instance"
-            @edit="editTunnel"
-            @delete="deleteTunnel"
-            @add-peer="addPeerHandler"
-            @edit-peer="editPeerHandler"
-            @delete-peer="deletePeerHandler"
-          />
-        </template>
-        <NeEmptyState
-          v-else
-          :title="t('standalone.wireguard_tunnel.no_server_configured')"
-          :description="t('standalone.wireguard_tunnel.no_server_configured_description')"
-          :icon="faGlobe"
-        >
-          <NeButton kind="primary" @click="showTunnelDrawer = true">
-            <template #prefix>
-              <FontAwesomeIcon :icon="faCirclePlus" aria-hidden="true" class="h-4 w-4" />
-            </template>
-            {{ t('standalone.wireguard_tunnel.add_server') }}
-          </NeButton>
-        </NeEmptyState>
-      </div>
-    </div>
+    <WireguardServerTunnelList v-if="selectedTab == 'server'" />
   </div>
-  <TunnelDrawer
-    :is-shown="showTunnelDrawer"
-    :tunnel="editingTunnel"
-    @success="createdTunnel"
-    @close="closeTunnelDrawer"
-  />
-  <DeleteTunnelModal
-    :instance="deletingTunnel"
-    @success="tunnelDeleted"
-    @close="closeDeleteModal"
-  />
-  <PeerDrawer
-    :is-shown="showPeerDrawer"
-    :instance="creatingPeerFor"
-    :peer="editPeer"
-    @close="closePeerDrawer"
-    @success="addedPeerHandler"
-  />
-  <DeletePeerModal :peer="peerToDelete" @success="peerDeleted" @close="closeDeletePeerModal" />
 </template>

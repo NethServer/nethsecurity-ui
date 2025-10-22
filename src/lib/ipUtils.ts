8e5cb025d1ac0ea3ef5a77f81e8bcb1936283892
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2025-10-22 10:04:15 +0200

    fixed lease order

---
+++ src/lib/ipUtils.ts
@@ -0,0 +1,10 @@
+export function ipv4ToInt(ip: string): number {
+  if (
+    !/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(
+      ip
+    )
+  ) {
+    throw new Error('Invalid IPv4 address')
+  }
+  return ip.split('.').reduce((acc, val) => acc * 256 + parseInt(val), 0)
+}

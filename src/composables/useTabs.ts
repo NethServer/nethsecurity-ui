102677743cb9bbaba2828b13613259560e7f0eb3
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2026-01-21 12:28:58 +0100

    refactored useTabs, using useRouteQuery from vueuse

--- src/composables/useTabs.ts
+++ src/composables/useTabs.ts
@@ -2,41 +2,19 @@
 //  SPDX-License-Identifier: GPL-3.0-or-later
 
 import type { Tab } from '@nethesis/vue-components'
-import { ref, watch } from 'vue'
-import { useRoute, useRouter } from 'vue-router'
+import { ref } from 'vue'
+import { useRouteQuery } from '@vueuse/router'
 
 /**
  * Composable that handles the state associated to the tabs of a page, binding them to
  * the router's `tab` query string.
- * @param {Tab[]} tabsList - A list containing all the tabs.
- * @param {string} [initialTabName] - If present, `selectedTab` will be set to
+ * @param {Tab[]} tabsList - A list containing all the tabs, the first tab will be selected by default.
  * the specified value when the component is mounted. If absent, the first value in `tabsList` will be
  * used instead.
  */
-export function useTabs(tabsList: Tab[], initialTabName?: string) {
-  const route = useRoute()
-  const router = useRouter()
-
+export function useTabs(tabsList: Tab[]) {
   const tabs = ref(tabsList)
-  const selectedTab = ref('')
-  const currentPath = route.path
-
-  watch(
-    () => route.query.tab,
-    () => {
-      if (route.path === currentPath) {
-        selectedTab.value =
-          (route.query.tab as string) ??
-          initialTabName ??
-          (tabs.value.length > 0 ? (tabs.value[0]?.name ?? '') : '')
-      }
-    },
-    { immediate: true }
-  )
-
-  watch(selectedTab, () => {
-    router.push({ path: route.path, query: { tab: selectedTab.value } })
-  })
+  const selectedTab = useRouteQuery('tab', tabs.value[0]?.name ?? '')
 
   return { tabs, selectedTab }
 }

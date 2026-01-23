cd4c484da51ab4c43e57499462ee4251d9d9f375
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2026-01-23 16:00:29 +0100

    added details

--- src/components/standalone/monitoring/flows/FlowBadge.vue
+++ src/components/standalone/monitoring/flows/FlowBadge.vue
@@ -1,18 +1,41 @@
 <script setup lang="ts">
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { NeBadgeV2 } from '@nethesis/vue-components'
+import { NeBadgeV2, NeTooltip } from '@nethesis/vue-components'
 import type { Badge } from '@/components/standalone/monitoring/flows/FlowTableRow.vue'
+import { useI18n } from 'vue-i18n'
+import { computed, h } from 'vue'
 
-defineProps<{
+const { t } = useI18n()
+
+const props = defineProps<{
   badge: Badge
 }>()
+
+const badgeComponent = computed(() =>
+  h(
+    NeBadgeV2,
+    {
+      customKindClasses: props.badge.customClasses.join(' '),
+      kind: 'custom',
+      size: 'sm'
+    },
+    () =>
+      h('span', { class: 'flex items-center gap-1' }, [
+        h(FontAwesomeIcon, { icon: props.badge.icon }),
+        t(props.badge.text)
+      ])
+  )
+)
 </script>
 
 <template>
-  <NeBadgeV2 :custom-kind-classes="badge.customClasses.join(' ')" kind="custom" size="sm">
-    <span class="flex items-center gap-1">
-      <FontAwesomeIcon :icon="badge.icon" />
-      {{ badge.text }}
-    </span>
-  </NeBadgeV2>
+  <NeTooltip v-if="badge.content" trigger-event="mouseenter click">
+    <template #content>
+      {{ t(badge.content) }}
+    </template>
+    <template #trigger>
+      <component :is="badgeComponent" />
+    </template>
+  </NeTooltip>
+  <component :is="badgeComponent" v-else />
 </template>

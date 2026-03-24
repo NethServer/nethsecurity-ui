c94c62522fca6d19a9c0121babecec43bcb71f46
Tommaso Bailetti <tommaso.bailetti@nethesis.it>
2026-03-24 16:00:56 +0100

    chore: repo maintainance

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,7 @@
+import { globalIgnores } from 'eslint/config'
 import pluginVue from 'eslint-plugin-vue'
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import pluginVitest from '@vitest/eslint-plugin'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
 export default defineConfigWithVueTs(
@@ -7,12 +9,13 @@ export default defineConfigWithVueTs(
     name: 'app/files-to-lint',
     files: ['**/*.{ts,mts,tsx,vue}']
   },
-  {
-    name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**']
-  },
-  pluginVue.configs['flat/recommended'],
+  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+  ...pluginVue.configs['flat/recommended'],
   vueTsConfigs.recommended,
+  {
+    ...pluginVitest.configs.recommended,
+    files: ['src/**/__tests__/*']
+  },
   {
     rules: {
       '@typescript-eslint/no-explicit-any': ['warn']
